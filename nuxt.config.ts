02f431db0d4be469b1eadb520b00599f0969b189
lmfundora <lmfundora2@gmail.com>
2025-05-05 14:52:03 -0400

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,29 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from '@primeuix/themes/aura';
+import tailwindcss from "@tailwindcss/vite";
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/fonts',
+    '@nuxt/image',
+    '@primevue/nuxt-module',
+    '@vueuse/nuxt',
+    '@pinia/nuxt',
+    'nuxt-zod-i18n',
+    '@nuxtjs/i18n'
+  ],
+  css: ['~/assets/css/main.css'],
+  vite: {
+      plugins: [
+        tailwindcss(),
+      ],
+    },
+  primevue: {
+      options: {
+          theme: {
+              preset: Aura
+          }
+      }
+  }
+})
