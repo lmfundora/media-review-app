28a8cb6f119b05af19e4c7fc187148dc1ad76e78
lmfundora <lmfundora2@gmail.com>
2025-05-07 09:10:43 -0400

    almost complete google singIn

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,10 +1,11 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "../db/schema"; // your drizzle instance
+import { db, user, session, account, verification } from "../db"; // your drizzle instance
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg", // or "mysql", "sqlite"
+    schema: { user, session, account, verification },
   }),
   emailAndPassword: {
     enabled: true,
--- pages/login/index.vue
+++ pages/login/index.vue
@@ -2,30 +2,14 @@
 import { authClient } from "@/lib/auth-client"; //import the auth client
 
 async function signInWithGoogle() {
+  console.log("siuu");
+
   await authClient.signIn.social({
-    /**
-     * The social provider id
-     * @example "github", "google", "apple"
-     */
     provider: "google",
-    /**
-     * A URL to redirect after the user authenticates with the provider
-     * @default "/"
-     */
     callbackURL: "/home",
-    /**
-     * A URL to redirect if an error occurs during the sign in process
-     */
     errorCallbackURL: "/error",
-    /**
-     * A URL to redirect if the user is newly registered
-     */
     newUserCallbackURL: "/welcome",
-    /**
-     * disable the automatic redirect to the provider.
-     * @default false
-     */
-    disableRedirect: true,
+    disableRedirect: false,
   });
 }
 </script>
