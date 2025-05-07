8b4b442a4233299ba98b293785235e36a358e96f
Luis David <lmfundora2@gmail.com>
2025-05-07 08:48:05 -0400

    try to integrate google logIn

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,5 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { authClient } from "@/lib/auth-client"; //import the auth client
+
+async function signInWithGoogle() {
+  await authClient.signIn.social({
+    /**
+     * The social provider id
+     * @example "github", "google", "apple"
+     */
+    provider: "google",
+    /**
+     * A URL to redirect after the user authenticates with the provider
+     * @default "/"
+     */
+    callbackURL: "/home",
+    /**
+     * A URL to redirect if an error occurs during the sign in process
+     */
+    errorCallbackURL: "/error",
+    /**
+     * A URL to redirect if the user is newly registered
+     */
+    newUserCallbackURL: "/welcome",
+    /**
+     * disable the automatic redirect to the provider.
+     * @default false
+     */
+    disableRedirect: true,
+  });
+}
+</script>
 <template>
-  <p>logIn</p>
+  <Button @click="signInWithGoogle"> Sign in with Google </Button>
 </template>
 <style scoped></style>
